355641ee3f14b86f96b1270c205ce5d83301a4dd
ParkSoyoung77 <fst704g@naver.com>
2026-09-07 10:12:46 +0900

    Feat: 테라폼 코드

--- 9.4/modules/storage/main.tf
+++ 9.4/modules/storage/main.tf
@@ -10,8 +10,8 @@ resource "aws_s3_bucket" "terraform_state" {
     }
 }
 
-resource "aws_s3_bucket_versioing" "state_versioning" {
-    bucket = aws_s3_buck.terraform_state.id
+resource "aws_s3_bucket_versioning" "state_versioning" {
+    bucket = aws_s3_bucket.terraform_state.id
     versioning_configuration {
         status = "Enabled"
     }
@@ -25,11 +25,13 @@ resource "aws_dynamodb_table" "terraform_lock" {
     # DynamoDB의 관리방식(비용과 연관된 설정)
     billing_mode = "PROVISIONED"    #PAY_PER_REQUEST
 
-    read_capcity   = 20 # 초당 4KB 데이터 읽기(RCU) => 1RCU
+    read_capacity   = 20 # 초당 4KB 데이터 읽기(RCU) => 1RCU
     write_capacity = 20 # 초당 4KB 데이터 쓰기(WCU) => 1WCU
 
     attribute {
         name = "LockID" # 관계형 데이터베이스의 P/K와 같은 역할
         type = "S"      # S(String), N(Number), B(Binary)
     }
+
+    hash_key = "LockID"
 }
