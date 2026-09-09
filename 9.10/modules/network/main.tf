51b306f302403806553e1ee70373a81afb9ecc5d
ParkSoyoung77 <fst704g@naver.com>
2026-09-09 16:49:12 +0900

    Feat: 테라폼 코드

--- 9.10/modules/network/main.tf
+++ 9.10/modules/network/main.tf
@@ -11,3 +11,22 @@ resource "aws_vpc" "this" {
         Name = "${local.tag_header}-vpc"
     }
 }
+
+resource "aws_subnet" "this"{
+    for_each = local.subnet_map
+
+    vpc_id            = aws_vpc.this.id
+    availability_zone = each.value.az
+    cidr_block        = each.value.cidr
+
+    map_public_ip_on_launch = each.value.type == "public" ? true :false
+
+    enable_resource_name_dns_a_record_on_launch = true
+
+    tags = merge({
+        "Name"        = "${local.tag_header}${each.key}-subnet"
+        "Type"        = each.value.type
+        "Environment" = "test"
+        "ManagedBy"   =  "Terraform"
+    })
+}
--- 9.10/modules/storage/main.tf
+++ 9.10/modules/storage/main.tf
@@ -1,38 +1,38 @@
-# =========================================================
-# 상태 잠금용 DynamoDB 테이블 생성
-# =========================================================
-resource "aws_s3_bucket" "terraform_state" {
-    bucket = "std17-instructor-terraform-state-bucket"
+# # =========================================================
+# # 상태 잠금용 DynamoDB 테이블 생성
+# # =========================================================
+# resource "aws_s3_bucket" "terraform_state" {
+#     bucket = "std17-instructor-terraform-state-bucket"
 
-    lifecycle {
-        prevent_destroy = false 
-    }
+#     lifecycle {
+#         prevent_destroy = false 
+#     }
 
-    tags={
-        Name = "std17-ex-terraform-state-bucket"
-    }
-}
+#     tags={
+#         Name = "std17-ex-terraform-state-bucket"
+#     }
+# }
 
-resource "aws_s3_bucket_versioning" "state_versioning" {
-    bucket = aws_s3_bucket.terraform_state.id
-    versioning_configuration {
-        status = "Enabled"
-    }
-}
+# resource "aws_s3_bucket_versioning" "state_versioning" {
+#     bucket = aws_s3_bucket.terraform_state.id
+#     versioning_configuration {
+#         status = "Enabled"
+#     }
+# }
 
-resource "aws_dynamodb_table" "terraform_lock" {
-    name = "std17-lab-lock-table"
-    # DynamoDB의 관리방식(비용과 연관된 설정)
-    billing_mode = "PROVISIONED"    # PAY_PER_REQUEST
+# resource "aws_dynamodb_table" "terraform_lock" {
+#     name = "std17-lab-lock-table"
+#     # DynamoDB의 관리방식(비용과 연관된 설정)
+#     billing_mode = "PROVISIONED"    # PAY_PER_REQUEST
 
-    hash_key = "LockID"
+#     hash_key = "LockID"
 
-    read_capacity   = 20 # 초당 4KB 데이터 읽기(RCU) => 1RCU
-    write_capacity = 20 # 초당 4KB 데이터 쓰기(WCU) => 1WCU
+#     read_capacity   = 20 # 초당 4KB 데이터 읽기(RCU) => 1RCU
+#     write_capacity = 20 # 초당 4KB 데이터 쓰기(WCU) => 1WCU
 
-    attribute {
-        name = "LockID" # 관계형 데이터베이스의 P/K와 같은 역할
-        type = "S"      # S(String), N(Number), B(Binary)
-    }
+#     attribute {
+#         name = "LockID" # 관계형 데이터베이스의 P/K와 같은 역할
+#         type = "S"      # S(String), N(Number), B(Binary)
+#     }
 
-}
+# }
