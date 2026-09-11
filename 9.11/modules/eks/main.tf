3c43a7524533a1cd44d3af319cafab403009116e
ParkSoyoung77 <fst704g@naver.com>
2026-09-11 16:47:38 +0900

    Feat: 테라폼 코드

--- 9.11/modules/eks/main.tf
+++ 9.11/modules/eks/main.tf
@@ -227,8 +227,8 @@ resource "aws_eks_node_group" "eks_node_group" {
     subnet_ids = var.private_subnet_ids
 
     scaling_config {
-        desired_size = 2
-        max_size     = 3
+        desired_size = 1
+        max_size     = 2
         min_size    = 1
     }
 
