23a4b0953b4a6b588ec7cfe42386797ab7e5db5f
ParkSoyoung77 <fst704g@naver.com>
2026-09-18 17:02:04 +0900

    Feat: 테라폼 코드

--- 9.18/provider.tf
+++ 9.18/provider.tf
@@ -24,6 +24,16 @@ terraform {
     # }
 }
 
+provider "aws"{
+    region = "ap-northeast-3"
+    default_tags {
+        tags = {
+            Class = "bipa17"
+            Owner = "std17"
+        }
+    }
+}
+
 # CodeStar Connections(GitHub 연동)가 오사카 리전에서 지원되지 않아
 # 도쿄 리전용 provider alias를 추가로 정의합니다.
 provider "aws" {
